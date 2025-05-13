8c5593ad5996b5ddb9d02611230b84cd2cda1af8
zkaakakag <cscom222@naver.com>
2025-05-13 16:02:46 +0900

    docker deploy4

--- src/main/java/com/example/instargram_server/AppController.java
+++ src/main/java/com/example/instargram_server/AppController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class AppController {
     @GetMapping("/")
     public String home() {
-        return "Hello, World! 2";
+        return "Hello, World! docker deploy";
     }
 }
