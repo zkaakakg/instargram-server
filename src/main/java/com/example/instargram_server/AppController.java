1d890f948cf9f321d9faa17f50c330467e4e57ab
zkaakakag <cscom222@naver.com>
2025-05-13 13:43:28 +0900

    first commit

---
+++ src/main/java/com/example/instargram_server/AppController.java
@@ -0,0 +1,12 @@
+package com.example.instargram_server;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+    @GetMapping("/")
+    public String home() {
+        return "Docker, World!";
+    }
+}
---
+++ src/main/java/com/example/instargram_server/InstargramServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.instargram_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstargramServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstargramServerApplication.class, args);
+	}
+
+}
