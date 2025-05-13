42c3ebf816e613af8d45674ebd8d187d22779a1b
zkaakakag <cscom222@naver.com>
2025-05-13 15:18:07 +0900

    commit

--- src/main/java/com/example/instargram_server/AppController.java
+++ src/main/java/com/example/instargram_server/AppController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class AppController {
     @GetMapping("/")
     public String home() {
-        return "Hello, World!";
+        return "Hello, World! 2";
     }
 }
